6c669917139a49bd7e480b58abfba34c2b9dcf79
Stanislav Ursache <d34dkn16h7@gmail.com>
2016-09-09 17:38:37 +0300

    upd

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use std::io::Read;
 use std::io::Write;
 
 use std::env;
+use std::path::Path;
 
+#[allow(dead_code)]
 struct ID3 {
     id3: bool,
     version: (u8, u8),
@@ -18,41 +20,57 @@ struct ID3 {
 }
 
 fn main() {
-    let args = env::args();
+    let mut args = env::args();
 
-    if args.len() > 0 {
-        let file_name = args.last().unwrap();
+    if args.len() > 2 {
+        let param = args.nth(1).unwrap();
+        let f_name = args.last().unwrap();
 
-        match File::open(&file_name)
-        {
-            Ok(mut file) => {
-                let mut whole_file = Vec::new();
-
-                match file.read_to_end(& mut whole_file){
-                    Ok(f_size) => {
-                        println!("File size for '{}' is : {} B", file_name ,f_size );
-
-                        let mut id3 = ID3::new();
-
-                        id3.from_vec(& mut whole_file, f_size);
-
-                        if id3.is_id3() {
-                            print!("ID3 -> ");
-                            id3.info();
-
-                            id3.create_folders();
-                            id3.write_file(&whole_file);
-                        }
-                        else {
-                            println!("Not ID3");
-                        }
-                    },
-                    Err(e) => println!("{}", e ),
+        match param.as_ref() {
+            "folder" => {
+                if Path::new( &f_name ).is_dir() {
+                    for music in read_dir(&f_name).unwrap() {
+                        match music {
+                            Ok( m ) => {
+                                let path = m.path();
+                                if path.extension().unwrap() == "mp3" {
+                                    analize_file( path.as_path() ).info();
+                                }
+                            },
+                            Err( e ) => println!("Error : {}" , e ) ,
+                        };
+                    }
                 }
+                else { println!("Not a folder"); }
             },
-            Err(e) => println!("{}" , e),
+            "file" => analize_file ( Path::new(&f_name) ).info() ,
+            _ =>  println!( "Wrong param" ),
         }
     }
+    else { println!("Enter params : .exe (file | folder)  name"); }
+}
+
+fn analize_file(file_name : &Path) -> ID3
+{
+    let mut id3 = ID3::new();
+
+    match File::open(&file_name)
+    {
+        Ok(mut file) => {
+            let mut whole_file = Vec::new();
+
+            match file.read_to_end(& mut whole_file){
+                Ok(f_size) => {
+                    id3.from_vec(& mut whole_file, f_size);
+                    id3.folder_name = file_name.to_string_lossy().into_owned();
+                },
+                Err(e) => println!("File reading error {}" , e),
+            }
+        },
+        Err(e) => println!("File error {}" , e) ,
+    }
+
+    return id3;
 }
 
 #[allow(dead_code)]
@@ -62,7 +80,9 @@ impl ID3 {
     }
 
     pub fn info(&self) {
-        println!("Version is {}.{}", self.version.0, self.version.1);
+        println!("File -> {}", self.folder_name );
+        print!("ID3  -> ");
+        println!("V {}.{}", self.version.0, self.version.1);
 
         println!("\tArtist is -> {}", self.artist);
         println!("\tAlbum is  -> {}", self.album);
@@ -70,13 +90,15 @@ impl ID3 {
         println!("\tTrack is  -> {}", self.track);
     }
 
-    pub fn create_folders(& mut self){
+    pub fn create_folders(& mut self) -> &mut ID3 {
         self.folder_name = format!("Music_Lib/{}/{}", self.artist.replace(" ",""), self.album.replace(" ",""));
 
         match create_dir_all(&self.folder_name){
             Ok(_) => println!("Path created : {}", self.folder_name),
             Err(e) => println!("{}", e),
         }
+
+        self
     }
 
     pub fn write_file(& mut self, vec: & Vec<u8>){
@@ -100,8 +122,8 @@ impl ID3 {
 
         copy_from_vec(& mut header, vec, 0, 3);
         copy_from_vec(& mut artist, vec, size - 95, 30);
-        copy_from_vec(& mut album, vec, size - 65, 30);
-        copy_from_vec(& mut title, vec, size - 125, 30);
+        copy_from_vec(& mut album,  vec, size - 65, 30);
+        copy_from_vec(& mut title,  vec, size - 125, 30);
 
         self.id3 = String::from_utf8(header.iter().cloned().collect()).unwrap() == "ID3";
         self.version = (vec[3], vec[4]);
